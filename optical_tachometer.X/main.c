1fc9a9dde1e966900120b7408032d953763783f4
Sandra Ekholm <svekho@utu.fi>
2020-12-09 13:53:22 +0200

    Kommenttei

--- optical_tachometer.X/main.c
+++ optical_tachometer.X/main.c
@@ -29,17 +29,15 @@ void calibrate_threshold(void);
 
 // Global variable to store adc result
 volatile uint16_t adcValue;
-// Global variable to store the rpm calculated from rotations
-uint16_t rpm;
 // Variable to count rotations
-uint16_t rotations;
+volatile uint16_t rotations;
 // Global variable msd (indicates most significant digit
 // value of rotating propeller rpm)
 uint8_t msd;
 // int to check if the propeller is in front of the LDR, and makes sure, the 
 // propeller is only counted once
 volatile uint8_t isPropOn;
-// Checking whether should update display
+// Checking whether should update display or calculate rotations from adc
 volatile uint8_t segmentUpdate;
 // Value for storing the voltage level used as indicator whether there is a 
 // propellor in front of the LDR or not.
@@ -190,8 +188,7 @@ ISR(ADC0_RESRDY_vect)
     ADC0.INTFLAGS = ADC_RESRDY_bm;
     // Setting the value adc measured
     adcValue = ADC0.RES;
-    //AdcValue: propeller is in front of LDR (is 700 originally)
-    // (will be replaced later is now magic number)
+    //AdcValue: propeller is in front of LDR, calibrated in the beginning
     if (adcValue>voltThreshold)
     {
         //makes sure the rotations are only updated once per rotation
@@ -240,7 +237,8 @@ int main(void)
     // In the beginning the value of parameters is 0
     adcValue = 0;
     rotations = 0;
-    rpm = 0;
+    // Variable to store the rpm calculated from rotations
+    uint16_t rpm = 0;
     msd = 0;
     isPropOn = 0;
     segmentUpdate = 0;
