1e8d36c4cf200887b6d156edbc7e5fdeb9a34672
Sandra Ekholm <svekho@utu.fi>
2020-12-15 17:01:19 +0200

    TCB konfigurointi

--- optical_tachometer_bonus1.X/update_spin.c
+++ optical_tachometer_bonus1.X/update_spin.c
@@ -2,5 +2,9 @@
 
 void update_spin(int userVoltage)
 {
-    ;
+    // Disable the peripheral
+    // Writing Capture/Compare register with new duty cycle based on
+    // potentiometer
+    // Reset count register
+    // Re-enable the module
 }
