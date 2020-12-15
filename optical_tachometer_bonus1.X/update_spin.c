29ca58cee3011d39f838a4da539a8304b12cb190
Sandra Ekholm <svekho@utu.fi>
2020-12-15 17:04:39 +0200

    update spinnii

--- optical_tachometer_bonus1.X/update_spin.c
+++ optical_tachometer_bonus1.X/update_spin.c
@@ -2,21 +2,11 @@
 
 void update_spin(int userVoltage)
 {
-<<<<<<< HEAD
     // Disable the peripheral
+    TCB3.CTRLA &= ~TCB_ENABLE_bm;
     // Writing Capture/Compare register with new duty cycle based on
     // potentiometer
     // Reset count register
     // Re-enable the module
-=======
-    if(userVoltage > 100)
-    {
-        VPORTA.OUT |= PIN2_bm;
-    }
-    else
-    {
-        VPORTA.OUT &= ~PIN2_bm;
-    }
-    
->>>>>>> bb4a5abb5fc67c6cece5486edd6147dde0ec48da
+    TCB3.CTRLA |= TCB_ENABLE_bm;
 }
